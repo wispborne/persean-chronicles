959fc7e2ea1bd8184e2d1cecede631ab915be167
David Whitman <whitmadm@gmail.com>
2021-01-18 09:44:01 -0800

    Gradle task to update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,6 +79,54 @@ tasks {
             listOf("./starsector.bat")
         }
     }
+
+    register("create-metadata-files") {
+        val version = javaslang.Tuple3("0", "7", "0")
+        val modId = "wisp_perseanchronicles"
+        val modName = "Persean Chronicles"
+        val author = "Wispborne"
+        val description = "Adds a small collection of quests to bars around the Persean Sector."
+        val gameVersion = "0.9.1a"
+        val jars = arrayOf("jars/PerseanChronicles.jar")
+        val modPlugin = "org.wisp.stories.LifecyclePlugin"
+        val isUtilityMod = false
+        val masterVersionFile = "https://raw.githubusercontent.com/davidwhitman/stories/master/$modId.version"
+        val modThreadId = "000000"
+
+        File(projectDir, "mod_info.json")
+            .writeText(
+                """
+                    {
+                        "id": "$modId",
+                        "name": "$modName",
+                        "author": "$author",
+                        "utility": "$isUtilityMod",
+                        "version": "${listOf(version._1, version._2, version._3).joinToString(separator = ".")}",
+                        "description": "$description",
+                        "gameVersion": "$gameVersion",
+                        "jars":[${jars.joinToString() { "\"$it\"" }}],
+                        "modPlugin":"$modPlugin"
+                    }
+                """.trimIndent()
+            )
+
+        File(projectDir, "$modId.version")
+            .writeText(
+                """
+                    {
+                        "masterVersionFile":"$masterVersionFile",
+                        "modName":"$modName",
+                        "modThreadId":$modThreadId,
+                        "modVersion":
+                        {
+                            "major":${version._1},
+                            "minor":${version._2},
+                            "patch":${version._3}
+                        }
+                    }
+                """.trimIndent()
+            )
+    }
 }
 
 // Compile to Java 6 bytecode so that Starsector can use it
