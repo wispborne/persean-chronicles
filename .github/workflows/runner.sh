e67bcee2e729143d0aecdf90f597e4ee02db5626
David <whitmadm@gmail.com>
2021-04-13 18:59:47 -0700

    Fixing this to actually zip the artifact, hopefully

--- .github/workflows/runner.sh
+++ .github/workflows/runner.sh
@@ -2,9 +2,9 @@
 
 ### Edit
 # Your mod name. The version number will be attached to this to form "My-Mod-1.0.0"
-MOD_FOLDER_NAME=My-Mod
+MOD_FOLDER_NAME=Persean-Chronicles
 ###
 
 
 chmod +x ./zipMod.sh
-./zipMod.sh "$MOD_FOLDER_NAME"
+sh ./zipMod.sh "./../.." "$MOD_FOLDER_NAME"
--- .github/workflows/zipMod.sh
+++ .github/workflows/zipMod.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
 # USAGE
-# ./zipMod.sh "mod-folder-name"
-#   arg mod-folder-name: Do not append the version, it will be appended.
+$ Run from within the 
+# ./zipMod.sh "folderToZip" "outputFolderName"
+#   arg folderToZip: The folder containing mod_info.json.
+#     ex: "D:/Code/Persean-Chronicles"
+#   arg outputFolderName: Do not append the version, it will be appended.
 #     ex: "Persean-Chronicles"
 
-modFolderName="$1"
+folderToZip=$1
+outputFolderName=$2
+startingDir=$(pwd)
 
+realpath $1
+
+cd $folderToZip
 version=$(git describe --tags)
-zipName=$modFolderName-$version.zip
+zipName=$outputFolderName-$version.zip
 
 # Recreate the temp folder if it happens to be present
-rm -rf "./$modFolderName-$version"
-mkdir "$modFolderName-$version"
+rm -rf "./$outputFolderName-$version"
+mkdir "$outputFolderName-$version"
 
 # 1. List all files in git, which uses gitignore
 # 2. Remove any file matching the blacklist (eg afphoto files)
 # 3. Copy to a new folder with the mod name and version
-git ls-files | grep -Evf ".circleci/blacklist.txt" | while read file; do cp --parents "$file" "$modFolderName-$version"; done
+git ls-files | grep -Evf "$startingDir/blacklist.txt" | while read file; do cp --parents "$file" "$outputFolderName-$version"; done
 
 # Zip the folder, then clean it up
-zip -r $zipName "./$modFolderName-$version"
-rm -rf "./$modFolderName-$version"
+zip -r $zipName "./$outputFolderName-$version"
+rm -rf "./$outputFolderName-$version"
 
 # Move the zip to the artifacts folder
 mkdir -p ./artifacts
