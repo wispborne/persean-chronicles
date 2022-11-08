2482393e51221d87ca4d4034a37270d9d0825094
Wisp <1343347+wispborne@users.noreply.github.com>
2022-11-07 19:37:10 -0800

    Lunge works now.

--- src/main/kotlin/wisp/perseanchronicles/telos/boats/TelosLungeSystem.kt
+++ src/main/kotlin/wisp/perseanchronicles/telos/boats/TelosLungeSystem.kt
@@ -1,21 +1,20 @@
 package wisp.perseanchronicles.telos.boats
 
-import com.fs.starfarer.api.combat.*
+import com.fs.starfarer.api.Global
+import com.fs.starfarer.api.combat.MutableShipStatsAPI
+import com.fs.starfarer.api.combat.PhaseCloakSystemAPI
+import com.fs.starfarer.api.combat.ShipAPI
+import com.fs.starfarer.api.impl.campaign.ids.Stats
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript
-import com.fs.starfarer.api.input.InputEventAPI
+import com.fs.starfarer.api.impl.combat.PhaseCloakStats
 import com.fs.starfarer.api.plugins.ShipSystemStatsScript
-import com.fs.starfarer.api.util.IntervalUtil
 import wisp.perseanchronicles.game
-import wisp.questgiver.wispLib.IntervalUtil
 import java.awt.Color
 
 class TelosLungeSystem : BaseShipSystemScript() {
     val speedBoost = 1000f
-
-
-//    override fun isUsable(system: ShipSystemAPI, ship: ShipAPI): Boolean {
-//        return super.isUsable(system, ship) && !ship.isPhased
-//    }
+    private var previousTimestamp: Float? = null
+    private var timeSinceStart = 0f
 
     override fun apply(
         stats: MutableShipStatsAPI,
@@ -23,9 +22,16 @@ class TelosLungeSystem : BaseShipSystemScript() {
         state: ShipSystemStatsScript.State,
         effectLevel: Float
     ) {
+        if (Global.getCombatEngine().isPaused) return
         val ship = stats.entity as ShipAPI
-        stats.maxSpeed.modifyFlat(id, speedBoost)
-        stats.acceleration.modifyFlat(id, speedBoost)
+
+        if (state == ShipSystemStatsScript.State.IN) {
+            previousTimestamp = game.combatEngine.getTotalElapsedTime(false)
+        }
+
+        timeSinceStart += (game.combatEngine.getTotalElapsedTime(false) - previousTimestamp!!)
+        previousTimestamp = game.combatEngine.getTotalElapsedTime(false)
+        game.logger.i { "Time since start: $timeSinceStart." }
 
         if (state == ShipSystemStatsScript.State.IN) {
             ship.allWeapons
@@ -33,35 +39,109 @@ class TelosLungeSystem : BaseShipSystemScript() {
                 .mapNotNull { it.effectPlugin }
                 .filterIsInstance<TelosPhaseEngines>()
                 .forEach { it.baseNebulaColorOverride = Color.decode("#F065FF") }
+
+            stats.maxSpeed.modifyFlat(id, speedBoost)
+            stats.acceleration.modifyFlat(id, speedBoost)
+
+//            val phaseAlpha =
+//                Easing.Quadratic.easeIn(
+//                    time = timeSinceStart,
+//                    valueAtStart = 1f,
+//                    valueAtEnd = 0f,
+//                    duration = 200f
+//                )
+            TelosPhase.apply(stats, id, state, effectLevel)
+        }
+
+
+        if (state == ShipSystemStatsScript.State.ACTIVE) {
+            stats.maxSpeed.unmodifyFlat(id)
+
+            // Set phase engine color to phase color
+            ship.allWeapons
+                .orEmpty()
+                .mapNotNull { it.effectPlugin }
+                .filterIsInstance<TelosPhaseEngines>()
+                .forEach { it.baseNebulaColorOverride = Color.decode("#9648ff") }
+
+            TelosPhase.apply(stats, id, state, effectLevel)
         }
 
         if (state == ShipSystemStatsScript.State.OUT) {
-            stats.maxSpeed.unmodifyFlat(id)
+            TelosPhase.apply(stats, id, state, effectLevel)
 
+            // Unset phase engine color
             ship.allWeapons
                 .orEmpty()
                 .mapNotNull { it.effectPlugin }
                 .filterIsInstance<TelosPhaseEngines>()
                 .forEach { it.baseNebulaColorOverride = null }
-
-            if (!ship.isPhased) {
-                ship.isPhased = true
-                game.combatEngine.addPlugin(object : BaseEveryFrameCombatPlugin() {
-                    val phaseDuration = IntervalUtil(2f)
-                    override fun advance(amount: Float, events: List<InputEventAPI>?) {
-                        phaseDuration.advance(amount)
-                        if (phaseDuration.intervalElapsed()) {
-                            ship.isPhased = false
-                            game.combatEngine.removePlugin(this)
-                        }
-                    }
-                })
-            }
         }
     }
 
     override fun unapply(stats: MutableShipStatsAPI, id: String) {
         stats.maxSpeed.unmodifyFlat(id)
         stats.acceleration.unmodifyFlat(id)
+        TelosPhase.unapply(stats, id)
+    }
+}
+
+object TelosPhase {
+    fun apply(
+        stats: MutableShipStatsAPI,
+        id: String,
+        state: ShipSystemStatsScript.State,
+        effectLevel: Float
+    ) {
+        val ship = stats.entity as? ShipAPI ?: return
+        val isPlayer = ship === Global.getCombatEngine().playerShip
+
+        val speedPercentMod = stats.dynamic.getMod(Stats.PHASE_CLOAK_SPEED_MOD).computeEffective(0f)
+        val accelPercentMod = stats.dynamic.getMod(Stats.PHASE_CLOAK_ACCEL_MOD).computeEffective(0f)
+        stats.maxSpeed.modifyPercent(id, speedPercentMod * effectLevel)
+        stats.acceleration.modifyPercent(id, accelPercentMod * effectLevel)
+        stats.deceleration.modifyPercent(id, accelPercentMod * effectLevel)
+
+        val speedMultMod = stats.dynamic.getMod(Stats.PHASE_CLOAK_SPEED_MOD).getMult()
+        val accelMultMod = stats.dynamic.getMod(Stats.PHASE_CLOAK_ACCEL_MOD).getMult()
+        stats.maxSpeed.modifyMult(id, speedMultMod * effectLevel)
+        stats.acceleration.modifyMult(id, accelMultMod * effectLevel)
+        stats.deceleration.modifyMult(id, accelMultMod * effectLevel)
+
+        val jitterLevel = 0f
+        val jitterRangeBonus = 0f
+        val levelForAlpha = effectLevel
+
+        ship.isPhased = true
+        ship.extraAlphaMult = 1f - (1f - PhaseCloakStats.SHIP_ALPHA_MULT) * levelForAlpha
+        ship.setApplyExtraAlphaToEngines(true)
+
+        val extra = 0f
+        val shipTimeMult = 1f + (PhaseCloakStats.getMaxTimeMult(stats) - 1f) * levelForAlpha * (1f - extra)
+        stats.timeMult.modifyMult(id, shipTimeMult)
+        if (isPlayer) {
+            Global.getCombatEngine().timeMult.modifyMult(id, 1f / shipTimeMult)
+        } else {
+            Global.getCombatEngine().timeMult.unmodify(id)
+        }
+    }
+
+    fun unapply(stats: MutableShipStatsAPI, id: String) {
+        val ship = stats.entity as? ShipAPI ?: return
+
+        Global.getCombatEngine().timeMult.unmodify(id)
+        stats.timeMult.unmodify(id)
+        stats.maxSpeed.unmodify(id)
+        stats.maxSpeed.unmodifyMult(id + "_2")
+        stats.acceleration.unmodify(id)
+        stats.deceleration.unmodify(id)
+        ship.isPhased = false
+        ship.extraAlphaMult = 1f
+        var cloak = ship.phaseCloak
+        if (cloak == null) cloak = ship.system
+
+        if (cloak != null) {
+            (cloak as? PhaseCloakSystemAPI)?.minCoilJitterLevel = 0f
+        }
     }
 }
