96275c5aa006e9ed0f7ec9224d868851fd2a4ed5
Wisp <1343347+wispborne@users.noreply.github.com>
2023-02-03 22:17:08 -0800

    Added short Eugel monologue and response to that.
    Also Luddic Church-aligned extra dialogue.

--- src/main/kotlin/wisp/perseanchronicles/telos/pt2_dart/Telos2SecondLandingDialog.kt
+++ src/main/kotlin/wisp/perseanchronicles/telos/pt2_dart/Telos2SecondLandingDialog.kt
@@ -1,6 +1,9 @@
 package wisp.perseanchronicles.telos.pt2_dart
 
+import com.fs.starfarer.api.campaign.RepLevel
 import com.fs.starfarer.api.characters.FullName
+import com.fs.starfarer.api.impl.campaign.ids.Factions
+import com.fs.starfarer.api.util.Misc
 import org.json.JSONObject
 import wisp.perseanchronicles.dangerousGames.pt1_dragons.DragonsHubMission
 import wisp.perseanchronicles.game
@@ -9,7 +12,6 @@ import wisp.perseanchronicles.telos.pt2_dart.battle.Telos2BattleCoordinator
 import wisp.perseanchronicles.telos.pt3_arrow.Telos3HubMission
 import wisp.questgiver.v2.InteractionDialogLogic
 import wisp.questgiver.v2.json.PagesFromJson
-import wisp.questgiver.v2.json.getPageById
 import wisp.questgiver.v2.json.query
 import wisp.questgiver.wispLib.findFirst
 
@@ -36,26 +38,44 @@ class Telos2SecondLandingDialog(
                 // Resume music
                 TelosCommon.playThemeMusic()
             },
+            "4-noPsi" to {
+                // You linger for a moment, thinking. You know the Church of Ludd well,
+                //              and recall an old firebrand named Eugel. Could it be the same man?\n\nYou shake your head and
+                //              follow Karengo to the hanger.
+                val page = navigator.currentPage()?.extraData!!
+                if (game.sector.playerFaction.isAtWorst(Factions.LUDDIC_CHURCH, RepLevel.COOPERATIVE)) {
+                    para { page["ludd-friendly"] as String }
+                    para { page["ludd-friendly2"] as String }
+                }
+            },
+            "6-ask" to {
+                // You linger for a moment, thinking. You know the Church of Ludd well,
+                //              and recall an old firebrand named Eugel. Could it be the same man?\n\nYou shake your head and
+                //              follow Karengo to the hanger.
+                val page = navigator.currentPage()?.extraData!!
+                if (game.sector.playerFaction.isAtWorst(Factions.LUDDIC_CHURCH, RepLevel.COOPERATIVE)) {
+                    para { page["ludd-friendly"] as String }
+                    para { page["ludd-friendly2"] as String }
+                }
+            },
             "5-noPsi" to {
                 // Resume music
                 TelosCommon.playThemeMusic()
             },
             // Manually show text based upon conditions.
             "9-check-karengo" to {
-                val page = getPageById(stageJson.query("/pages"), "9-check-karengo")
+                val page = navigator.currentPage()?.extraData!!
 
-                if (page != null) {
-                    if (game.sector.playerPerson.gender == FullName.Gender.FEMALE) {
-                        // how do i turn off this ship sense/feeling  i can feel you sitting on the couch not that i mind but it’s distracting
-                        para { page.optString("shipSense1-female") }
-                    } else {
-                        // how do i turn off this ship sense/feeling  i can feel you sitting on that couch and i’d rather not
-                        para { page.optString("shipSense1-notFemale") }
-                    }
-
-                    // The ship shows you how to limit the integration, but you can’t help but wonder what it would have felt like to be aboard at the height of the Telos.
-                    para { page.optString("shipSense2") }
+                if (game.sector.playerPerson.gender == FullName.Gender.FEMALE) {
+                    // how do i turn off this ship sense/feeling  i can feel you sitting on the couch not that i mind but it’s distracting
+                    para { page["shipSense1-female"] as String }
+                } else {
+                    // how do i turn off this ship sense/feeling  i can feel you sitting on that couch and i’d rather not
+                    para { page["shipSense1-notFemale"] as String }
                 }
+
+                // The ship shows you how to limit the integration, but you can’t help but wonder what it would have felt like to be aboard at the height of the Telos.
+                para { page["shipSense2"] as String }
             },
             // Without Ether
             "7.1-noPsi" to {
