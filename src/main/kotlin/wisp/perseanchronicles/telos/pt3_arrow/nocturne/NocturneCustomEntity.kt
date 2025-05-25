ef6fbe4b84f3c3441401f8575aeecef421057a3b
Wisp <1343347+wispborne@users.noreply.github.com>
2025-05-25 12:40:47 -0400

    Mostly fixed Ethersight dimming, but there's still the wonky dimming zooming bug. Think it's vanilla.

--- src/main/kotlin/wisp/perseanchronicles/telos/pt3_arrow/nocturne/NocturneCustomEntity.kt
+++ src/main/kotlin/wisp/perseanchronicles/telos/pt3_arrow/nocturne/NocturneCustomEntity.kt
@@ -28,22 +28,22 @@ class NocturneCustomEntity : BaseCustomEntityPlugin() {
         val uly = viewport.lly + viewport.visibleHeight
         val urx = viewport.llx + viewport.visibleWidth
 
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_BLEND)
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
 
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_TEXTURE_2D)
 
-        GL11.glColor4f(0f, 0f, 0f, alpha); // black with 65% opacity
+        GL11.glColor4f(0f, 0f, 0f, alpha) // black with 65% opacity
 
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2f(viewport.llx, viewport.lly);
-        GL11.glVertex2f(viewport.llx, uly);
-        GL11.glVertex2f(urx, uly);
-        GL11.glVertex2f(urx, viewport.lly);
-        GL11.glEnd();
+        GL11.glBegin(GL11.GL_QUADS)
+        GL11.glVertex2f(viewport.llx, viewport.lly)
+        GL11.glVertex2f(viewport.llx, uly)
+        GL11.glVertex2f(urx, uly)
+        GL11.glVertex2f(urx, viewport.lly)
+        GL11.glEnd()
 
-        GL11.glEnable(GL11.GL_TEXTURE_2D); // re-enable for rest of rendering
-        GL11.glColor4f(1f, 1f, 1f, 1f);     // reset color to white/opaque
+        GL11.glEnable(GL11.GL_TEXTURE_2D) // re-enable for rest of rendering
+        GL11.glColor4f(1f, 1f, 1f, 1f)     // reset color to white/opaque
 
     }
 }
